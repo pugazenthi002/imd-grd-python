3970788d5ee8166cd3e24cb0616d3bf250e8184b
Jeevanand Palanisamy <jeevanand0013@gmail.com>
2022-03-22 13:24:31 +0530

    added provision to enter paramters manually and to download singl eparamter or any combination

--- imd-down.py
+++ imd-down.py
@@ -1,13 +1,15 @@
 import imdlib as imd
 import csv
 import pandas as pd
-import os, glob
+import os, glob,time
 import os.path
 from os import path
 os.makedirs("temp", exist_ok=True)
 csv_file = 'latlon-input.csv'
 start_yr = int(input("Enter start year : "))
 end_yr = int(input("Enter end year(till 2020) : ")) 
+params = input("Enter paramters seperated by space (eg: tmax tmin rain) :" ) or "tmax tmin rain"
+pars = params.split()
 lats=[]
 lons=[]
 with open(csv_file, 'r') as f:
@@ -15,7 +17,7 @@ with open(csv_file, 'r') as f:
     for row in reader:
         lats.append(float(row.get('lat')))
         lons.append(float(row.get('lon')))
-pars = ["rain","tmax","tmin"]
+#pars = ["tmax","tmin","rain"]
 for i in range(len(lats)):
     for variable in pars: 
         os.makedirs(variable, exist_ok=True)
@@ -37,14 +39,43 @@ for i in range(len(lats)):
         cb_var.to_csv("temp\\" + variable + '_' + str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv', index=False)
         for f in fl:
             os.remove(f)
-    f1=pd.read_csv("temp\\" + 'tmax_' + str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv')
-    f2=pd.read_csv("temp\\" + 'tmin_' + str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv')
-    f3=pd.read_csv("temp\\" + 'rain_' + str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv')
-    mer1=f1.merge(f2,on='DateTime')
-    mer2=mer1.merge(f3,on='DateTime')
-    mer2.to_csv(str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv',header=['Date','Tmax','Tmin','Rain'],index=False)
-        
-
+    tmaxf=True
+    tminf=True
+    rainf=True
+    try:
+        f1=pd.read_csv("temp\\" + 'tmax_' + str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv')
+    except FileNotFoundError: 
+        print("tmax not needed")
+        tmaxf = False
+    try:
+        f2=pd.read_csv("temp\\" + 'tmin_' + str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv')
+    except FileNotFoundError:
+        print("tmin not needed")
+        tminf = False
+    try:
+        f3=pd.read_csv("temp\\" + 'rain_' + str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv')
+    except FileNotFoundError:
+        print("tmax not needed")
+        rainf = False
+    if (tmaxf) and (tminf) and (rainf):    
+        mer1=f1.merge(f2,on='DateTime')
+        mer2=mer1.merge(f3,on='DateTime')
+        mer2.to_csv(str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv',header=['Date','Tmax','Tmin','Rain'],index=False)
+    elif (not tmaxf) and (tminf) and (rainf):
+        mer2=f2.merge(f3,on='DateTime')
+        mer2.to_csv(str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv',header=['Date','Tmin','Rain'],index=False)
+    elif (tmaxf) and (not tminf) and (rainf):
+        mer1=f1.merge(f3,on='DateTime')
+        mer1.to_csv(str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv',header=['Date','Tmax','Rain'],index=False)
+    elif (not tmaxf) and (not tminf) and (rainf):
+        print("only-rain")
+        f3.to_csv(str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv',header=['Date','Rain'],index=False)
+    elif (tmaxf) and (tminf) and (not rainf):
+        mer1=f1.merge(f2,on='DateTime')
+        mer1.to_csv(str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv',header=['Date','Tmax','Tmin'],index=False)
+    else:
+        print("nothing to do")
+        exit
     filelist = glob.glob(os.path.join("*_"+ str(f'{lats[i]:.2f}') + '_' + str(f'{lons[i]:.2f}') + '.csv'))
     for f in filelist:
         os.remove(f)
